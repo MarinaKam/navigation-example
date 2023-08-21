e32f0a265cb45a3d6cbeb50920c6f3e41843e78b
Maryna Kamysheva <marina.stozhok@gmail.com>
2023-08-20 20:03:37 -0600

    fix: babel settings.

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { UserScreen } from './screens/UserScreen';
+import { WelcomeScreen } from './screens/WelcomeScreen';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Todo...</Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+
+      <Text>Todo</Text>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="User" component={UserScreen} />
+          <Drawer.Screen name="Article" component={WelcomeScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
